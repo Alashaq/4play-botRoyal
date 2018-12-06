975c9961fef7f8ab26f17f9ba7e950a39d299ad1
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-12-06 10:41:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,32 @@ client.on('ready', () => {
 
             }
 }); 
-
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'chat');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "chat")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` invited By :  ${Invite.inviter} ♥ `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 client.on('message', message => {
     if(message.channel.type === 'dm') {
         var guildID = '490300993519747079'; // <=============== ايدي السيرفر حقك
